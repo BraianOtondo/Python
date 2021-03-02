2622010ae2e82264e9bc7ff720eb511d57e23499
Braian <braian_otondo99@outlook.com>
2021-03-02 18:26:39 -0300

    Se agregó mas cosas a la carpeta de Operadores

--- Operadores/operadores.py
+++ Operadores/operadores.py
@@ -3,4 +3,7 @@ num2=2
 resultado=num1+num2
 print(resultado)
 print(type(resultado)) #Esto es para ver el tipo de dato de la variable
-print(num1**num2) #Esto es una potencia 
+print(num1**num2) #Esto es una potencia
+print(20//2) #Esto es una division
+print(20%2) #Con esto se saca el resto
+print(2+(2-2)) #Esto debe dar 2
