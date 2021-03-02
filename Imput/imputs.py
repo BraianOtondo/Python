a328e06584f640de8bb9a6a7e0b87e8fb2af0755
Braian <braian_otondo99@outlook.com>
2021-03-02 19:15:32 -0300

    Se agrega un nuevo método de input

--- Imput/imputs.py
+++ Imput/imputs.py
@@ -1,4 +1,6 @@
 nombre=input("Ingrese su nombre: ")
+edad=int(input("Ingrese su edad: ") )
 print(f"Tu nombre es : { nombre }")
 cantidad = int(input("Escriba número entero: "))#Se puede especificar int y float
 print ("El entero es: ",cantidad)
+print("Enhorabuena, tu edad es de "+ str(edad))
